10866077a49a69f62fd8af239f6df94df86ac0de
Ali Bouzar <alibouzar@foi.hr>
2022-01-10 09:20:18 +0100

    Integrating FeignClient with Eureka

--- student-service/src/main/java/com/miaoubich/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/miaoubich/feignclients/AddressFeignClient.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.miaoubich.response.AddressResponse;
 
 //To call address service via url
-//value, is this current feign client name which we called it address-feign
-@FeignClient(url = "${address.service.url}", value = "address-feign")
+//value: is this current feign client name which we called it address-feign (call it whatever you like)
+//Before registering the Address microservice with eureka we use its url
+//@FeignClient(url = "${address.service.url}", value = "address-feign", path="/api/address") 
+//After registering Address microservice with eurka we use its name as a value
+@FeignClient(value = "address-service", path="/api/address") 
 public interface AddressFeignClient {
 
 	@GetMapping("/{addressId}")
